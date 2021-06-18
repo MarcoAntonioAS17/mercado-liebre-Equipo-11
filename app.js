8fc6a5afe157c2e81a2d58ff85000a5db7013910
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-06-17 19:38:13 -0500

    Puerto del servidor

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-//const PORT = process.env.PORT || 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
+
 app.use(express.static(path.resolve(__dirname, './public')) );
 
 
